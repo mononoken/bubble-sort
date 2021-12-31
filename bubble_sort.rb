e65e41146ed334dc47b9a923d87bc3c849ca27df
mononoken <ken.maeshima@pm.me>
2021-12-31 12:58:28 -0600

    Fix methods such that they do not alter original array. Bubble sort functions as intended.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,21 @@
 def swap_elements(array, index1, index2)
-  array[index1], array[index2] = array[index2], array[index1]
-  array
+  swapped_array = array.dup
+  swapped_array[index1], swapped_array[index2] = swapped_array[index2], swapped_array[index1]
+  swapped_array
 end
 
 def bubble_once(array)
-  bubbled_array = array
+  bubbled_array = array.dup
   bubbled_array.each_with_index do |left_num, left_index|
     if  
       right_index = left_index + 1
-      right_num = array[right_index]
+      right_num = bubbled_array[right_index]
 
       if right_index == bubbled_array.length
         break
       else
         if right_num < left_num
-          swap_elements(bubbled_array, left_index, right_index)
+          bubbled_array = swap_elements(bubbled_array, left_index, right_index)
         end
       end
     end
@@ -22,6 +23,13 @@ def bubble_once(array)
   bubbled_array
 end
 
+def bubble_sort(unsorted_array)
+  while unsorted_array != bubble_once(unsorted_array)
+    unsorted_array = bubble_once(unsorted_array)
+  end
+  unsorted_array
+end
+
 test_array = [4,3,78,2,0,2]
 p test_array
-p bubble_once(test_array)
+p bubble_sort(test_array)
